eb2889f20baa4295b591913a1416fd2348cfce81
Sujith <ps335@snu.edu.in>
2024-04-08 19:30:49 +0530

    serverside config

--- pages/meow.tsx
+++ pages/meow.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
 import { useSession } from 'next-auth/react'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/auth'
+import type { GetServerSideProps,GetServerSidePropsContext } from 'next'
 
-function meow() {
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+    const session = await getServerSession(context.req, context.res, authOptions);
+    
+    return {
+        props: { user: session?.user ?? null }
+    }
+}
+
+function meow({ user }) {
     const { data: session, status } = useSession()
 
     return (
         <div>
             {status === "loading" && <div>Loading...</div>}
             {status === "authenticated" && <div>Meow, {session.user.email}</div>}
+            {user && <div>Meow, {user.email}</div>}
             {status === "unauthenticated" && <div>Meow, stranger</div>}
         </div>
     )
