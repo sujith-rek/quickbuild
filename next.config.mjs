91b242603d7dd75132d836d52ff1df6b615b6e7c
Sujith <ps335@snu.edu.in>
2024-04-08 19:13:38 +0530

    scss cofig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,26 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 /** @type {import('next').NextConfig} */
+import path from "path";
+
+// Get the directory name of the current module
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const nextConfig = {
   reactStrictMode: true,
-};
-
+  swcMinify: true,
+  webpack(config) {
+    config.module.rules.forEach((rule) => {
+      const { oneOf } = rule;
+      if (oneOf) {
+        oneOf.forEach((one) => {
+          if (!`${one.issuer?.and}`.includes("_app")) return;
+          one.issuer.and = [path.resolve(__dirname)];
+        });
+      }
+    });
+    return config;
+  },
+}
 export default nextConfig;
--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -4,16 +4,21 @@ import { hashSync } from "bcrypt";
 import { PrimitiveUser } from "@/types/types";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
     if (req.method === "POST") {
+
         const user: PrimitiveUser = req.body;
         user.password = hashSync(user.password, 10);
+
         try {
             const newUser = await createUser(user);
             res.status(201).json(newUser);
         } catch (error) {
             res.status(500).json({ message: "Internal server error" });
         }
+
     } else {
         res.status(405).json({ message: "Method not allowed" });
     }
+
 }
