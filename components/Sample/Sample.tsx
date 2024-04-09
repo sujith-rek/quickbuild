d360461cd5787614ba6583f973dd0b68b6137697
Sujith <ps335@snu.edu.in>
2024-04-09 23:58:13 +0530

    add neko

--- components/Sample/Sample.tsx
+++ components/Sample/Sample.tsx
@@ -2,30 +2,49 @@ import { FaGithub } from "react-icons/fa";
 import { SiPrisma, SiChakraui } from "react-icons/si";
 import { Icon } from "@chakra-ui/react";
 import "./Sample.scss";
+import { Neko } from "neko-ts";
+import { useEffect, useRef } from "react";
 
 function Sample() {
+
+    const neko = useRef<Neko>();
+
+    useEffect(() => {
+        if (!neko.current) {
+            neko.current = new Neko({
+                origin: {
+                    x: 100,
+                    y: 100,
+                },
+            });
+        }
+    }, [neko]);
+
     return (
-        <div className="Sample">
-            <h1>
-                This is a NextJS boilerplate with TypeScript, SCSS, NextAuth
-            </h1>
-            <p>
-                Features included:
-            </p>
-            <ul>
-                <li><Icon as={SiPrisma} /> Prisma ORM</li>
-                <li><Icon as={SiChakraui} /> Chakra UI</li>
-                <li>NextAuth</li>
-                <li>SCSS</li>
-                <li>NodeMailer</li>
-                <li>React Icons</li>
-            </ul>
-            <br />
-            <br />
-            <span>
-                Made with ❤️ by <a href="https://github.com/sujith-rek"><Icon as={FaGithub} /> Sujith</a>
-            </span>
-        </div>
+        <>
+            <div className="Sample">
+                <h1>
+                    This is a NextJS boilerplate with TypeScript, SCSS, NextAuth
+                </h1>
+                <p>
+                    Features included:
+                </p>
+                <ul>
+                    <li><Icon as={SiPrisma} /> Prisma ORM</li>
+                    <li><Icon as={SiChakraui} /> Chakra UI</li>
+                    <li>NextAuth</li>
+                    <li>SCSS</li>
+                    <li>NodeMailer</li>
+                    <li>React Icons</li>
+                </ul>
+                <br />
+                <br />
+                <span>
+                    Made with ❤️ by <a href="https://github.com/sujith-rek"><Icon as={FaGithub} /> Sujith</a>
+                </span>
+            </div>
+            <canvas id="canvas"></canvas>
+        </>
     );
 }
 
